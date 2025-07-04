3fef1715cf96e4dc52c154782adb3c2a145d4c61
MatheusHenriqueCIN <159284886+MatheusHenriqueCIN@users.noreply.github.com>
2025-07-04 01:16:23 -0300

    Add files via upload

---
+++ back/src/handlers/GeneroFilmeHandlers.h
@@ -0,0 +1,20 @@
+#ifndef GENERO_FILME_HANDLERS_H
+#define GENERO_FILME_HANDLERS_H
+
+#include <cpprest/http_listener.h>
+#include "GeneroFilme.h"
+#include <vector>
+#include <mutex>
+
+extern std::vector<GeneroFilme> genero_filmes;
+extern std::mutex genero_filme_mutex;
+
+void inicializar_genero_filmes();
+void criar_genero_filme(const web::http::http_request& request);
+void listar_genero_filmes(const web::http::http_request& request);
+void deletar_genero_filme(const web::http::http_request& request);
+
+bool carregar_genero_filmes();
+bool salvar_genero_filmes();
+
+#endif // GENERO_FILME_HANDLERS_H
